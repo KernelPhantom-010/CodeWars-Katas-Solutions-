04a6ad560e8bdd1165da11e3dcf4df3785832a0c
Fabio Baensch <wiggle.koala@icloud.com>
2026-07-11 11:00:46 +0200

    Create Oh_come_on_Hello_world_once_again.cpp

---
+++ Solutions20262027/Oh_come_on_Hello_world_once_again.cpp
@@ -0,0 +1,18 @@
+
+/*
+Link: https://www.codewars.com/kata/594a7ea087a7c6cbe60000d6/train/cpp
+Description:
+
+Write a function f (in C# F) that returns Hello, world! string. But there is one thing, you cannot use helowrd letters.
+
+The C# test cases ignore checking for banned letters in words class, string and public.
+The C++ test cases ignore checking for banned letters in words string, std and return.
+The Julia test cases ignore checking for banned letters in words module, Solution, export and end.
+
+Have fun!
+------------------------------------------------------------------------------------------------------------------------
+*/
+std::string f()
+{
+    return "\110\145\154\154\157\54\40\167\157\162\154\144\41";
+}
